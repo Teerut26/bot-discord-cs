7d6df27d35be530e0b8237046f92d40e7bc4a349
Teerut26 <teerutsr@gmail.com>
2022-06-07 23:32:30 +0700

    fix intents deploy

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,15 @@ declare module "discord.js" {
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_INVITES,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES, // <= Don't miss this :)
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
     ],
 });
 
