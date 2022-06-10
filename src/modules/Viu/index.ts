181dca7d6011f5849c1ec59122a40100863e7b66
Teerut26 <teerutsr@gmail.com>
2022-06-10 09:27:10 +0700

    fix api url

--- src/modules/Viu/index.ts
+++ src/modules/Viu/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Grid, ViuAPIResponse } from "./interface/ViuAPIResponse";
+import { ViuAPIResponse } from "./interface/ViuAPIResponse";
 require("dotenv").config();
 
 export default class Viu {
@@ -7,23 +7,11 @@ export default class Viu {
     private base_url: string = process.env.VIU_BASE_URL!;
 
     public async excute() {
-        const { data } = await axios(
-            `${this.base_url}/home/index&platform_flag_label=phone&language_flag_id=4&ut=0&area_id=4`,
-            {
-                headers: {
-                    Platform: "android",
-                    Authorization: `Bearer ${process.env.VIU_TOKEN}`,
-                    "Accept-Encoding": "gzip, deflate",
-                    "User-Agent": "okhttp/3.12.1",
-                },
-            }
-        );
+        const { data } = await axios.get("https://www.viu.com/ott/th/index.php?r=listing/ajax&platform_flag_label=web&area_id=4&language_flag_id=4&cpreference_id=&grid_id=180804");
         this.response = data;
     }
 
-    public getRanking(): Grid {
-        return this.response?.data.grid.filter(
-            (item) => item.grid_id === "180806"
-        )[0]!;
+    public getRanking():ViuAPIResponse {
+        return this.response!
     }
 }
